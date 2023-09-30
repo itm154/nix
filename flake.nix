5b1d7d581524fa25230150726d85ba19f848fa86
itm154 <ashrulfahmi@gmail.com>
2023-09-30 16:27:30 +0800

    bruh

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "NixOs flakes";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -15,6 +14,7 @@
     self,
     nixpkgs,
     home-manager,
+    hyprland,
     ...
   } @ inputs: let
     inherit (self) outputs;
@@ -37,6 +37,8 @@
       itm154-nix = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [
+          hyprland.nixosModules.default
+          {programs.hyprland.enable = true;}
           ./nixos/configuration.nix
         ];
       };
--- home-manager/dekstop/wayland/hyprland/config.nix
+++ home-manager/dekstop/wayland/hyprland/config.nix
@@ -4,29 +4,29 @@
   config,
   ...
 }: {
-  wayland.windowManager.hyprland.settings = {
-    general = {
-      gaps_in = 4;
-      gaps_out = 12;
-      border_size = 2;
-
-      "col.active_border" = "#F38BA8";
-      "col.inactive_border" = "#F38BA8";
-    };
-
-    decoration = {
-       active_opacity = 1.0;
-      inactive_opacity = 0.85;
-      fullscreen_opacity = 1.0;
-      rounding = 12;
-
-      blur = {
-        enabled = true;
-        size = 5;
-        passes = 3;
-        new_optimizations = true;
-        ignore_opacity = false;
-      };
-    };
-  };
+  # wayland.windowManager.hyprland.settings = {
+  #   general = {
+  #     gaps_in = 4;
+  #     gaps_out = 12;
+  #     border_size = 2;
+  #
+  #     "col.active_border" = "#F38BA8";
+  #     "col.inactive_border" = "#F38BA8";
+  #   };
+  #
+  #   decoration = {
+  #      active_opacity = 1.0;
+  #     inactive_opacity = 0.85;
+  #     fullscreen_opacity = 1.0;
+  #     rounding = 12;
+  #
+  #     blur = {
+  #       enabled = true;
+  #       size = 5;
+  #       passes = 3;
+  #       new_optimizations = true;
+  #       ignore_opacity = false;
+  #     };
+  #   };
+  # };
 }
