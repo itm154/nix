2efef9df584fb128b2279adc80b0f38a28312063
itm154 <ashrulfahmi@gmail.com>
2023-10-02 17:08:36 +0800

    Mako border radius

--- home-manager/features/desktop/wayland/mako.nix
+++ home-manager/features/desktop/wayland/mako.nix
@@ -7,6 +7,7 @@
     backgroundColor = "#${base00}";
     borderColor = "#${base08}";
     borderSize = 2;
+    borderRadius = 12;
     output = "eDP-1";
     textColor = "#${base05}";
     font = "JetBrains Mono Nerd Font 16";
