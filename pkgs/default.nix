24c40f98c809e1d2e07c9da6d79b3612005f2d8d
itm154 <ashrulfahmi@gmail.com>
2023-10-03 12:50:12 +0800

    nvchad

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,9 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
-pkgs: {
-  # example = pkgs.callPackage ./example { };
-  nvchad = pkgs.callPackage ./nvchad { };
+# pkgs: {
+#   # example = pkgs.callPackage ./example { };
+#   nvchad = pkgs.nvchad ./nvchad { };
+# }
+{ pkgs ? (import ../nixpkgs.nix) {}}: {
+  nvchad = pkgs.callPackage ./nvchad {};
 }
