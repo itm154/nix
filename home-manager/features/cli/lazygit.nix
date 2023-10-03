ba2bd38ec269b8137892ff28c6d6b1ce9ba1323c
itm154 <ashrulfahmi@gmail.com>
2023-10-03 13:46:02 +0800

    Fix lazygit settigns

--- home-manager/features/cli/lazygit.nix
+++ home-manager/features/cli/lazygit.nix
@@ -1,20 +1,22 @@
 { config, ...}: {
   programs.lazygit = with config.colorScheme.colors; {
     enable = true;
-    gui.theme = {
-      lightTheme = false;
-      activeBorderColor = [ "#${base0B}" "bold" ];
-      inactiveBorderColor = [ "#${base05}" ];
-      optionsTextColor = [ "#${base0D}" ];
-      selectedLineBgColor = [ "#${base02}"];
-      selectedRangeBgColor = [ "#${base02}" ];
-      cherryPickedCommitBgColor = [ "#${base0C}" ];
-      cherryPickedCommitFgColor = [ "#${base05}" ];
-      unstagedChangesColor = [ "#${base08}" ];
-    };
-    gui = {
-      showIcons = true;
-      showBottomLine =  false;
+    settings = {
+      gui.theme = {
+        lightTheme = false;
+        activeBorderColor = [ "#${base0B}" "bold" ];
+        inactiveBorderColor = [ "#${base05}" ];
+        optionsTextColor = [ "#${base0D}" ];
+        selectedLineBgColor = [ "#${base02}"];
+        selectedRangeBgColor = [ "#${base02}" ];
+        cherryPickedCommitBgColor = [ "#${base0C}" ];
+        cherryPickedCommitFgColor = [ "#${base05}" ];
+        unstagedChangesColor = [ "#${base08}" ];
+      };
+      gui = {
+        showIcons = true;
+        showBottomLine =  false;
+      };
     };
   };
 }
